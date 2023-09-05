37fcd7e93d472995a8ea81abdb34f9fadf19691d
summer030 <125527872+summer030@users.noreply.github.com>
2023-09-05 21:56:27 +0800

    feat: individual product

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { Expand, ShoppingCart } from "lucide-react";
 
 import { Product } from "@/types";
+
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
 
@@ -11,8 +13,15 @@ interface ProductCard {
   data: Product;
 }
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  };
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
       {/* Images and Acitons */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
