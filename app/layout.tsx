d1ebd59fdffca7eb9f34787d09be09a597a935f1
summer030 <125527872+summer030@users.noreply.github.com>
2023-09-08 22:55:57 +0800

    feat: product preview modal components

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Urbanist } from "next/font/google";
 
 import NavBar from "@/components/navbar";
 import Footer from "@/components/footer";
+import ModalProvider from "@/providers/modal-provider";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
         <NavBar />
         {children}
         <Footer />
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -3,19 +3,27 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Expand, ShoppingCart } from "lucide-react";
+import { MouseEventHandler } from "react";
 
 import { Product } from "@/types";
 
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 interface ProductCard {
   data: Product;
 }
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  const previewModal = usePreviewModal();
   const router = useRouter();
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
+  };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+    previewModal.onOpen(data);
   };
   return (
     <div
@@ -33,7 +41,7 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
